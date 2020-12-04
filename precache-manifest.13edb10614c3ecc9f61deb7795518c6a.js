31bb9ec26a1cadd4e618a3a200200bf944f11d7b
pantojao <61061684+pantojao@users.noreply.github.com>
2020-12-04 12:37:42 -0800

    Updates

---
+++ precache-manifest.13edb10614c3ecc9f61deb7795518c6a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4cf29765f497539ba492047aa61f532",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b194e23dfce595cdd402",
+    "url": "/portfolio/static/css/main.5792fa42.chunk.css"
+  },
+  {
+    "revision": "629b6b7c1792518bdfbd",
+    "url": "/portfolio/static/js/2.2b785ff5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.2b785ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b194e23dfce595cdd402",
+    "url": "/portfolio/static/js/main.b0431c44.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8542b367d97716ccbf31762e7b6d1272",
+    "url": "/portfolio/static/media/gainz-app-pic.8542b367.PNG"
+  },
+  {
+    "revision": "afd09fc2609a2597f5137fde98089821",
+    "url": "/portfolio/static/media/gainz-app-vid.afd09fc2.png"
+  },
+  {
+    "revision": "864b424a30ddb79a646b756a5e03ad91",
+    "url": "/portfolio/static/media/ipa-app-pic.864b424a.PNG"
+  },
+  {
+    "revision": "68e55f374520ffdd7cc8dcc50c831f05",
+    "url": "/portfolio/static/media/ipa-app-vid.68e55f37.png"
+  },
+  {
+    "revision": "1e5c3b8f725866ad51a8b384198a12b9",
+    "url": "/portfolio/static/media/myPhoto.1e5c3b8f.jpg"
+  },
+  {
+    "revision": "7939adf8b2e40caa67c557e26971a305",
+    "url": "/portfolio/static/media/nasa-app-pic.7939adf8.PNG"
+  },
+  {
+    "revision": "509b0120e1341461ef0ef9f1ae59746b",
+    "url": "/portfolio/static/media/nasa-app-vid.509b0120.png"
+  },
+  {
+    "revision": "a4b67f643f7c1181c5d52498dda77758",
+    "url": "/portfolio/static/media/resume-pic.a4b67f64.PNG"
+  },
+  {
+    "revision": "7255c36c48f6d57b8fca8cff67d32fcb",
+    "url": "/portfolio/static/media/tree.7255c36c.jpeg"
+  },
+  {
+    "revision": "468b954830e7f79e0c35bc0f82f80e2b",
+    "url": "/portfolio/static/media/weather-app-pic.468b9548.PNG"
+  },
+  {
+    "revision": "7ee1526b1ebbbf2f96c3e3f1f27e0191",
+    "url": "/portfolio/static/media/weather-app-vid.7ee1526b.png"
+  }
+]);
