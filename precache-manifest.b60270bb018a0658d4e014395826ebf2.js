b7688cb27dbba6f43493159bd917f87c4d414a63
pantojao <61061684+pantojao@users.noreply.github.com>
2020-12-08 14:34:01 -0800

    Updates

---
+++ precache-manifest.b60270bb018a0658d4e014395826ebf2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d94a007f290401d477ab01421fca347",
+    "url": "/joelpantoja.com/index.html"
+  },
+  {
+    "revision": "a31a554678552dfddacf",
+    "url": "/joelpantoja.com/static/css/main.af25c620.chunk.css"
+  },
+  {
+    "revision": "7cbb9fe7c429b9a3874d",
+    "url": "/joelpantoja.com/static/js/2.322a53dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/joelpantoja.com/static/js/2.322a53dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31a554678552dfddacf",
+    "url": "/joelpantoja.com/static/js/main.772fdb63.chunk.js"
+  },
+  {
+    "revision": "9553832c9e21f31aabe2",
+    "url": "/joelpantoja.com/static/js/runtime-main.9041477f.js"
+  },
+  {
+    "revision": "8542b367d97716ccbf31762e7b6d1272",
+    "url": "/joelpantoja.com/static/media/gainz-app-pic.8542b367.PNG"
+  },
+  {
+    "revision": "afd09fc2609a2597f5137fde98089821",
+    "url": "/joelpantoja.com/static/media/gainz-app-vid.afd09fc2.png"
+  },
+  {
+    "revision": "864b424a30ddb79a646b756a5e03ad91",
+    "url": "/joelpantoja.com/static/media/ipa-app-pic.864b424a.PNG"
+  },
+  {
+    "revision": "68e55f374520ffdd7cc8dcc50c831f05",
+    "url": "/joelpantoja.com/static/media/ipa-app-vid.68e55f37.png"
+  },
+  {
+    "revision": "1e5c3b8f725866ad51a8b384198a12b9",
+    "url": "/joelpantoja.com/static/media/myPhoto.1e5c3b8f.jpg"
+  },
+  {
+    "revision": "7939adf8b2e40caa67c557e26971a305",
+    "url": "/joelpantoja.com/static/media/nasa-app-pic.7939adf8.PNG"
+  },
+  {
+    "revision": "509b0120e1341461ef0ef9f1ae59746b",
+    "url": "/joelpantoja.com/static/media/nasa-app-vid.509b0120.png"
+  },
+  {
+    "revision": "a4b67f643f7c1181c5d52498dda77758",
+    "url": "/joelpantoja.com/static/media/resume-pic.a4b67f64.PNG"
+  },
+  {
+    "revision": "7255c36c48f6d57b8fca8cff67d32fcb",
+    "url": "/joelpantoja.com/static/media/tree.7255c36c.jpeg"
+  },
+  {
+    "revision": "468b954830e7f79e0c35bc0f82f80e2b",
+    "url": "/joelpantoja.com/static/media/weather-app-pic.468b9548.PNG"
+  },
+  {
+    "revision": "7ee1526b1ebbbf2f96c3e3f1f27e0191",
+    "url": "/joelpantoja.com/static/media/weather-app-vid.7ee1526b.png"
+  }
+]);
